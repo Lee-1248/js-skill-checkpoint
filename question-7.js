7d5eaab555136d31462b45ee1c87bb825d96e693
Ratchapoom Tienchutima <zirconx14@gmail.com>
2024-01-21 19:29:43 +0700

    feat: add question 7

---
+++ question-7.js
@@ -0,0 +1,17 @@
+// Question #7: Array Merging
+
+const firstProductList = [
+    { id: 1, name: "Laptop", price: 1000 },
+    { id: 2, name: "Smartphone", price: 700 },
+  ];
+  
+  const secondProductList = [
+    { id: 3, name: "Tablet", price: 500 },
+    { id: 4, name: "Headphones", price: 150 },
+  ];
+
+  let newProductList;
+  newProductList = firstProductList.concat(secondProductList)
+  console.log(newProductList)
+  
+  
