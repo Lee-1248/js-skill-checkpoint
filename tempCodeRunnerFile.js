ae8bd4173c4f04110a8278c278c2f080ded89d16
Ratchapoom Tienchutima <zirconx14@gmail.com>
2024-01-21 17:57:07 +0700

    feat: add question 4

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,18 +1,2 @@
-const inventory = {
-  apple: { price: 35, quantity: 100 },
-  banana: { price: 10, quantity: 50 },
-};
-
-inventory.apple["quantity"] = 200;
-inventory.orange = {}
-inventory.orange.price = 20
-inventory.orange.quantity = 300
-
-
-let total = 0;
-
-for (let fruit in inventory){
-  total += inventory[fruit].price*inventory[fruit].quantity
-}
-
-console.log(`Total inventory value: ${total} baht`)
+  console.log(calculateShippingCost(orderTotal2)); // "Shipping cost is 250 Baht."
+  console.log(calculateShippingCost(orderTotal3)); // "Shipping cost is 500 Baht."
