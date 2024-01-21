6a7aa75e74c5cfb63463e5dd625d003894feef35
Ratchapoom Tienchutima <zirconx14@gmail.com>
2024-01-21 17:25:51 +0700

    add question 2

---
+++ question-2.js
@@ -0,0 +1,11 @@
+const userTaskList = [
+    { id: 1, task: "Buy groceries" },
+    { id: 2, task: "Finish homework" },
+    { id: 3, task: "Call mom" },
+    { id: 4, task: "Wash dishes" },
+  ];
+  
+userTaskList.push({ id: 5, task: "Walk the dog" })
+userTaskList[3].task = "Go to the gym"
+userTaskList.pop()
+console.log(userTaskList[3])
